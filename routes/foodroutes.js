7badc15522144804546eae82ba4f49f9862b97aa
Jesus <jjcorrales76@gmail.com>
2025-02-03 22:34:06 -0700

    removed foodcontroller file

--- routes/foodroutes.js
+++ routes/foodroutes.js
@@ -1,13 +1,31 @@
 import express from 'express';
-import foodController  from '../controllers/foodController.js';
+import Food from "../models/food.js"
 
 const router = express.Router();
 
-router.get('/', foodController.getFoods);
+router.get('/', async (req, res) => {
+    try {
+        const foods = await Food.find();
+        console.log(foods)
+        res.render('foods/index', { foods }); // Pass food data to the view
+    } catch (error) {
+        res.status(500).send("Error fetching food data");
+    }
+});
 
-router.get('/new', foodController.showAddForm)
 
-router.post('/', foodController.createFood);
+router.get('/new', (req, res) => {
+    res.render('foods/add');
+});
+
+router.post('/', async (req, res) => {
+    try {
+        await Food.create(req.body);
+        res.redirect('/foods'); 
+    } catch (error) {
+        res.status(500).send("Error adding food");
+    }
+});
 
 
 export default router;
